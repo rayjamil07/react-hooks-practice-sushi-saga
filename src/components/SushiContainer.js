71fd612255240333b9fcc277d76dfae121c7b1b9
rayjamil <ryanwekesa019@gmail.com>
2025-05-28 11:16:25 +0300

    incomplete commit

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -3,14 +3,14 @@ import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
 function SushiContainer({ sushis, onEatSushi, eaten }) {
-  const [sushiIndex, setSushiIndex] = useState(0);
+  const [ sushiIndex,setSushiIndex ] = useState(0);
 
   const sushiCard = sushis.slice(sushiIndex, sushiIndex + 4).map((sushi) => {
     return <Sushi
-      key={sushi.id}
-      sushi={sushi}
+      key={sushis.id}
+      sushi={sushis}
       onEatSushi={onEatSushi}
-      eaten={eaten.some(e => e.id === sushi.id)}
+      eaten={eaten.some(e => e.id === sushis.id)}
     />
   });
   
